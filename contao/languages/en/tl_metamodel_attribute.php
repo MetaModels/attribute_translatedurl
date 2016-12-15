87360ed35d536def437e8cef938399d1a43d6ff0
Christian Schiffler <c.schiffler@cyberspectrum.de>
2016-12-15 12:46:40 +0100

    Fix file headers

--- contao/languages/en/tl_metamodel_attribute.php
+++ contao/languages/en/tl_metamodel_attribute.php
@@ -1,24 +1,26 @@
 <?php
 
 /**
- * The MetaModels extension allows the creation of multiple collections of custom items,
- * each with its own unique set of selectable attributes, with attribute extendability.
- * The Front-End modules allow you to build powerful listing and filtering of the
- * data in each collection.
+ * This file is part of MetaModels/attribute_translatedurl.
  *
- * PHP version 5
+ * (c) 2012-2016 The MetaModels team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
  * @package    MetaModels
- * @subpackage AttributeUrl
+ * @subpackage AttributeTranslatedUrl
+ * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
  * @author     Stefan Heimes <stefan_heimes@hotmail.com>
  * @author     Andreas Isaak <info@andreas-isaak.de>
  * @author     Christopher Boelter <christopher@boelter.eu>
- * @copyright  The MetaModels team.
- * @license    LGPL.
+ * @copyright  2012-2016 The MetaModels team.
+ * @license    https://github.com/MetaModels/attribute_translatedurl/blob/master/LICENSE LGPL-3.0
  * @filesource
  */
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['typeOptions']['translatedurl'] = 'Translated-URL';
 
-/**
- * Fields
- */
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['typeOptions']['translatedurl']    = 'Translated-URL';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['trim_title']            = array('Remove title', 'If selected, only the link field is used.');
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['trim_title'][0] = 'Remove title';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['trim_title'][1] = 'If selected, only the link field is used.';
