818d6a80a32ff4bc53c99335ba387059ed0715e1
Mini Model <minimodel@metamodel.me>
2018-07-03 22:20:36 +0200

    Updated translations from transifex.

---
+++ contao/languages/et/tl_metamodel_attribute.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * @link https://www.transifex.com/signup/
+ * @link https://www.transifex.com/projects/p/$$project$$/language/$$lang$$/
+ *
+ * last-updated: 2018-07-03T22:20:33+02:00
+ */
+
+
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['trim_title']['0']              = 'Eemalda pealkiri';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['typeOptions']['translatedurl'] = ' URL';
+
