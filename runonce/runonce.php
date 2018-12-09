d9479f66fdb17f49941b57b86a6a7d331c4dad25
Christian Schiffler <c.schiffler@cyberspectrum.de>
2018-12-09 22:58:02 +0100

    Fix author validation.

--- runonce/runonce.php
+++ runonce/runonce.php
@@ -13,9 +13,6 @@
  * @package    MetaModels
  * @subpackage AttributeTranslatedUrl
  * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
- * @author     Andreas Isaak <info@andreas-isaak.de>
- * @author     Christopher Boelter <christopher@boelter.eu>
- * @author     Sven Baumann <baumann.sv@gmail.com>
  * @copyright  2012-2016 The MetaModels team.
  * @license    https://github.com/MetaModels/attribute_translatedurl/blob/master/LICENSE LGPL-3.0
  * @filesource
